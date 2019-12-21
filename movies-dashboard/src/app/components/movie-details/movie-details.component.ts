4775af51544d2d0cdb3d0643bfc8e903c0909235
BuceaDaniel <bucead@gmail.com>
2019-12-22 00:19:55 +0200

    1

--- movies-dashboard/src/app/components/movie-details/movie-details.component.ts
+++ movies-dashboard/src/app/components/movie-details/movie-details.component.ts
@@ -1,15 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
+import { ActivatedRoute } from "@angular/router";
+import { MovieAPIService } from "src/services/movie-api.service";
 
 @Component({
-  selector: 'app-movie-details',
-  templateUrl: './movie-details.component.html',
-  styleUrls: ['./movie-details.component.sass']
+  selector: "app-movie-details",
+  templateUrl: "./movie-details.component.html",
+  styleUrls: ["./movie-details.component.sass"]
 })
 export class MovieDetailsComponent implements OnInit {
+  private movieId: String;
+  private currentMovie: any = {};
+  private defaultPoster: String = "../../../assets/img/no-poster.jpg";
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(
+    private movieAPIService: MovieAPIService,
+    private activatedRoute: ActivatedRoute
+  ) {
+    this.movieId = this.activatedRoute.snapshot.paramMap.get("id");
   }
 
+  ngOnInit() {
+    this.movieAPIService.getMovie(this.movieId).subscribe(observer => {
+      this.currentMovie = observer;
+    });
+  }
 }
